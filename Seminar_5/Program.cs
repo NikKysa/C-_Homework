02d94d49d2f6a04e28c4d7bf8364a60615241b0c
Николай <sisse87@mail.ru>
2022-09-08 02:18:35 +0300

    Seminar_5_Homework

--- Seminar_5/Program.cs
+++ Seminar_5/Program.cs
@@ -12,27 +12,27 @@
 // Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма 
 // положительных чисел равна 29, сумма отрицательных равна -20.
 
-int[] array = new int[12];
-int size = array.Length;
+// int[] array = new int[12];
+// int size = array.Length;
 
-int positiveSum = 0;
-int negativeSum = 0;
+// int positiveSum = 0;
+// int negativeSum = 0;
 
-for (int i = 0; i < size; i++)
-{
-    array[i] = new Random().Next(-9, 10);
+// for (int i = 0; i < size; i++)
+// {
+//     array[i] = new Random().Next(-9, 10);
 
-    if (array[i] > 0)
-    {
-        positiveSum += array[i];
-    }
-    else
-    {
-        negativeSum += array[i];
-    }
-}
-Console.WriteLine("Сумма положительных чисел = " + positiveSum + " | Сумма отрицательных чисел" + negativeSum);
-Console.WriteLine(string.Join(";",array));
+//     if (array[i] > 0)
+//     {
+//         positiveSum += array[i];
+//     }
+//     else
+//     {
+//         negativeSum += array[i];
+//     }
+// }
+// Console.WriteLine("Сумма положительных чисел = " + positiveSum + " | Сумма отрицательных чисел" + negativeSum);
+// Console.WriteLine(string.Join(";",array));
 
 
 
---
+++ Seminar_5_Homework/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными 
+// трёхзначными числами. Напишите программу, которая покажет количество 
+// чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+// Console.WriteLine("Введите размер массива:");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Наш массив: ");
+// PrintArray(numbers);
+// int count = 0;
+
+// for (int i = 0; i < numbers.Length; i++)
+// if (numbers[i] % 2 == 0)
+// count++;
+
+// Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+// void FillArrayRandomNumbers(int[] numbers)
+// {
+//     for(int j = 0; j < numbers.Length; j++)
+//     {
+//         numbers[j] = new Random().Next(100,1000);
+//     }
+// }
+// void PrintArray(int[] numbers)
+// {
+//     Console.Write("[ ");
+//     for(int j = 0; j < numbers.Length; j++)
+//     {
+//         Console.Write(numbers[j] + " ");
+//     }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+// int [] array = { -4, -6, 89, 6 };
+// int oddNumSum = 0;
+
+// for (int i = 1; i < array.Length; i+=2)
+
+//     oddNumSum+=array[i];
+
+// Console.WriteLine("Сумма элементов на нечетных позициях = " + oddNumSum);
+
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между 
+// максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+// int[] array = { 5, 8, 98, 3, 24 };
+// int i, min, max, diff;
+// min = array[0];
+// max  = array[0];
+// diff = 0;
+
+// for (i = 0; i < array.Length; i++)
+// {
+//     if (array[i] > max)
+//     {
+//         max = array[i];
+//     }
+//     if (array[i] < min)
+//     {
+//         min = array[i];
+//     }
+//     diff = max - min;
+// }
+
+// Console.WriteLine("Разность между макс и мин = " + diff);
